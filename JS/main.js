5a75aa87e766f49573a5b4cfbc311e4b09a79758
EdoardaDepine <edoarda.depine@gmail.com>
2023-03-30 11:35:16 -0300

    implementando operações de soma e subtração aos botões do braço do robô, por meio do JS

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,18 @@
-const robotron = document.querySelector("#robotron");
+const subtrair = document.querySelector("#subtrair");
+const somar = document.querySelector("#somar");
+const braco = document.querySelector("#braco");
+const controle = document.querySelectorAll(".controle-ajuste");
 
-robotron.addEventListener("click", function () {
-  console.log("oi");
+controle.forEach((elemento) => {
+  elemento.addEventListener("click", (evento) => {
+    manipulaDados(evento.target.textContent);
+  });
 });
+
+function manipulaDados(operacao) {
+  if (operacao === "-") {
+    braco.value = parseInt(braco.value) - 1;
+  } else {
+    braco.value = parseInt(braco.value) + 1;
+  }
+}
